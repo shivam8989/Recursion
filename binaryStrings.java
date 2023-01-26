1517d384c2f5463eb46a6f34238e7d7611f2b94e
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-01-26 14:58:17 +0530

    Add files via upload
    Recursion in java

---
+++ binaryStrings.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class binaryStrings  {
+    public static void printBinaryString(int n , int lastPlace, String str) {
+
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+
+        printBinaryString(n - 1, 0, str+"0");
+        if (lastPlace == 0) {
+            printBinaryString(n - 1, 1, str+"1");
+
+
+        }
+    }
+        public static void main (String args []){
+        printBinaryString(2,0," ");
+        }
+
+
+
+}
